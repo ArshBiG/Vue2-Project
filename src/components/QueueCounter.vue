1bacb6c2d8484da6e584d80f37d5d3117c04e489
ArshBiG <dbugtm@gmail.com>
2026-09-07 19:24:11 +0330

    new

---
+++ src/components/QueueCounter.vue
@@ -0,0 +1,40 @@
+<script>
+
+export default {
+    data() {
+        return{
+            count: 1,
+        }
+    },
+    methods: {
+        increment(){
+            this.count++
+        },
+        decrement(){
+            this.count--
+        },
+        reset(){
+            this.count = 1
+        }
+    }
+}
+
+</script>
+
+
+<template>
+    <div>
+        <h1>Counter List</h1>
+        <h2>Number of turns : {{ count }}</h2>
+
+        <p>برای افزودن نوبت کلیک کنید</p>
+        <button @click="increment" >incerment</button>
+
+        <p>برای کاهش نوبت کلیک کنید</p>
+        <button @click="decrement" >decerment</button>
+
+        <p>برای ریست نوبت کلیک کنید</p>
+        <button @click="reset" >reset</button>
+
+    </div>
+</template>
---
+++ src/components/miniProject.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  data() {
+    return {
+      count : 0,
+      username : "",
+      imageUrl : "https://picsum.photos/400",
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    clearUsername() {
+      this.username = "";
+    }
+  }
+
+};
+
+// New
+
+</script>
+
+<template>
+  <div class="main" >
+    <!-- Event Binding Click -->
+    <h1>this is a simple Vue app</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Plus</button>
+
+    <!-- Attribute Binding v-bind -->
+    <div>
+      <img v-bind:src="imageUrl" alt="Random Image">
+    </div>
+
+    <!-- Data Binding v-model -->
+    <div>
+      <p v-if="username">
+        Username: {{ username }}
+      </p>
+      <p v-else>
+        وارد نشده
+      </p>
+      <input v-model="username" type="text">
+      <button @click="clearUsername">
+        Clear Username
+      </button>
+    </div>
+
+  </div>
+</template>
+
+
+<style lang="scss" >
+$primary-color: red;
+
+.main {
+  text-align: center;
+  margin-top: 50px;
+
+  h1 {
+    font-size: 40px;
+    color: $primary-color;
+  }
+}
+
+</style>
